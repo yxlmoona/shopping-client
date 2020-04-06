b26295393c208c8ec4643ca92e2d692df39e7ce7
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-04-06 17:54:52 -0500

    install redux, set up store

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+import store from './store.js'
 
 class App extends React.Component{
   render(){
     return(
+      <Provider store={store}>
       <h1>hi</h1>
+      </Provider>
     )
   }
 }
